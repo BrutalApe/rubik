33a8e789f353bd0cbd22098b029eecd521b26bd2
BrutalApe <75955942+BrutalApe@users.noreply.github.com>
2020-12-14 20:56:42 -0500

    Can now spin cube
    QWEASD rotate cube, updated executable

--- Main.cs
+++ Main.cs
@@ -5,6 +5,10 @@ public class Main : Node
 {
 
     PackedScene new_cube;
+    PackedScene new_cam;
+
+    private Camera cameraMain;
+    private Spatial cube;
 
     // Declare member variables here. Examples:
     // private int a = 2;
@@ -13,18 +17,19 @@ public class Main : Node
     // Called when the node enters the scene tree for the first time.
     public override void _Ready()
     {
+        //Input.SetMouseMode(Input.MouseMode.Captured);
+
         int cube_size = 3;
 
-        Spatial cube = addCube(cube_size);
-        
-        addCamera(cube_size);
+        cube = addCube(cube_size);        
+        cameraMain = addCamera(cube_size);
         
         Vector3 edge_select = new Vector3(0, 0, 1);
 
         twistCube(cube, cube_size, edge_select, 1, 1);
     }
 
-    public void addCamera(int cube_size)
+    public Camera addCamera(int cube_size)
     {
         Camera camera = new Camera{};
         AddChild(camera);
@@ -44,6 +49,8 @@ public class Main : Node
 
         camera.Translate(new_loc);
         camera.RotationDegrees = new_rot;
+
+        return camera;
     }
 
     public Spatial addCube(int cube_size)
@@ -61,10 +68,76 @@ public class Main : Node
         cube.Call("spinSide", size, side_select, direction, times);
     }
 
+    public void rotateCamera_x(int key_x)
+    {
+        cube.RotateX(key_x/(float)Math.PI/100);
 
-//  // Called every frame. 'delta' is the elapsed time since the previous frame.
-//  public override void _Process(float delta)
-//  {
-//      
-//  }
+        return;
+    }
+
+    public void rotateCamera_y(int key_y)
+    {
+        cube.RotateY(key_y/(float)Math.PI/100);
+
+        return;
+    }
+
+    public void rotateCamera_z(int key_z)
+    {
+        cube.RotateZ(key_z/(float)Math.PI/100);
+
+        return;
+    }
+
+    public override void _Input(InputEvent inputEvent)
+    {
+        if (inputEvent is InputEventKey keyEvent && keyEvent.Pressed)
+        {
+            if ((KeyList)keyEvent.Scancode == KeyList.A)
+            {
+                rotateCamera_x(1);
+            }
+            if ((KeyList)keyEvent.Scancode == KeyList.D)
+            {
+                rotateCamera_x(-1);
+            }
+            
+            if ((KeyList)keyEvent.Scancode == KeyList.S)
+            {
+                rotateCamera_y(1);
+            }
+            if ((KeyList)keyEvent.Scancode == KeyList.W)
+            {
+                rotateCamera_y(-1);
+            }
+            
+            if ((KeyList)keyEvent.Scancode == KeyList.E)
+            {
+                rotateCamera_z(1);
+            }
+            if ((KeyList)keyEvent.Scancode == KeyList.Q)
+            {
+                rotateCamera_z(-1);
+            }
+        }
+
+        return;
+    } 
+
+    private void ProcessInput(float delta)
+    {
+        //     if (Input.IsActionJustPressed("ui_cancel"))
+        // {
+        //     if (Input.GetMouseMode() == Input.MouseMode.Visible)
+        //         Input.SetMouseMode(Input.MouseMode.Captured);
+        //     else
+        //         Input.SetMouseMode(Input.MouseMode.Visible);
+        // }
+    }
+
+    // Called every frame. 'delta' is the elapsed time since the previous frame.
+    public override void _Process(float delta)
+    {
+        ProcessInput(delta);
+    }
 }
--- Readme.md
+++ Readme.md
@@ -1,4 +1,7 @@
 12/14/20
 Added Readme.md
 
-Features: Cube Generation, Side rotated. No user functionality.
+Features: Cube Generation, Side rotated.
+    Use A,D to rotate on X axis
+    Use W,S to rotate on Y axis
+    Use Q,E to rotate on Z axis
